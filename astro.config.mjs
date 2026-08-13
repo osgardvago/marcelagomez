f2fa43e15a95072c3f617393472caff888d159e6
osgardvago <vagoarslab@proton.me>
2026-08-12 20:53:10 -0600

    astro config
    output: 'static',

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
